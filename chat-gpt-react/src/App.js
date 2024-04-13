e57b7de43391282618928be125456808c5a45ec9
matthew rendely <matthew.rendely@gmail.com>
2024-04-13 13:40:39 -0700

    Clean up files

--- chat-gpt-react/src/App.js
+++ chat-gpt-react/src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import Chat from './components/Chat';
-import Header from './components/Header';
 
 export default function App() {
     return (
     <>
-        <Header />
-        <Chat />
-        <div class='input'>hello</div>
+       
     </>
     )
 }
